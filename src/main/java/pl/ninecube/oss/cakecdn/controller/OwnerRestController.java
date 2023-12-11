3c7c84e575e4d731bea13945f18f38517e008671
Daniel Pustuła <dpustula@ninecube.pl>
2023-12-11 10:00:05 +0100

    add swagger

--- src/main/java/pl/ninecube/oss/cakecdn/controller/OwnerRestController.java
+++ src/main/java/pl/ninecube/oss/cakecdn/controller/OwnerRestController.java
@@ -1,5 +1,7 @@
 package pl.ninecube.oss.cakecdn.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,27 +14,32 @@ import pl.ninecube.oss.cakecdn.service.OwnerService;
 @RestController
 @RequestMapping("/owner")
 @RequiredArgsConstructor
+@Tag(name = "Managing owners")
 public class OwnerRestController {
 
     private final OwnerService ownerService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Save new owner")
     public OwnerResponse createOwner(@Valid @RequestBody OwnerCreateDto dto) {
         return ownerService.saveOwner(dto);
     }
 
     @GetMapping("/{ownerId}")
+    @Operation(summary = "Get owner by id")
     public OwnerResponse readOwner(@PathVariable Long ownerId) {
         return ownerService.getOwnerById(ownerId);
     }
 
     @PutMapping("/{ownerId}")
+    @Operation(summary = "Update owner data by id")
     public OwnerResponse updateOwner(@PathVariable Long ownerId, OwnerUpdateDto dto) {
         return ownerService.updateOwnerById(ownerId, dto);
     }
 
-    @DeleteMapping("/ownerId")
+    @DeleteMapping("/{ownerId}")
+    @Operation(summary = "Delete owner by id")
     public void deleteOwner(@PathVariable Long ownerId) {
         ownerService.deleteOwner(ownerId);
     }
