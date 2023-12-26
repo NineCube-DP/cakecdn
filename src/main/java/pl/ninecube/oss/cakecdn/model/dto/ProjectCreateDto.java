a85b026db3de741a392b90c03543ef288497ee5d
Daniel Pustuła <dpustula@ninecube.pl>
2023-12-26 19:37:00 +0100

    add unique project base url, add validator for url

--- src/main/java/pl/ninecube/oss/cakecdn/model/dto/ProjectCreateDto.java
+++ src/main/java/pl/ninecube/oss/cakecdn/model/dto/ProjectCreateDto.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
+import org.hibernate.validator.constraints.URL;
 
 @Jacksonized
 @Builder
 @Value
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ProjectCreateDto {
-  String name;
-  String baseUrl;
+    String name;
+  
+    @URL
+    String baseUrl;
 }
